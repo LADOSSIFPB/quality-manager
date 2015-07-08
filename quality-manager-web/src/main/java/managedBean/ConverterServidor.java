5321428d2a1a1d3fb7d6dd6143282382f4f807c8
FelipeNascimennto <felipe_nsousa@outlook.com>
2015-07-07 23:53:35 -0300

    -Opção para adicionar coordenador já disponível em Adicionar Curso.  -Ajustes de alguns detalhes nos componentes

---
+++ quality-manager-web/src/main/java/managedBean/ConverterServidor.java
@@ -0,0 +1,49 @@
+package managedBean;
+
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.convert.Converter;
+import javax.faces.convert.FacesConverter;
+import javax.ws.rs.core.GenericType;
+import javax.ws.rs.core.Response;
+
+import br.edu.ifpb.qmanager.entidade.Pessoa;
+import br.edu.ifpb.qmanager.entidade.Servidor;
+
+@FacesConverter("converterServidor")
+public class ConverterServidor extends GenericBean implements Converter {
+
+	@Override
+	public Object getAsObject(FacesContext context, UIComponent componente,
+			String value) {
+
+		if (value != "0" && value != null) {
+
+			Response response = service
+					.consultarServidor(Integer.parseInt(value));
+			Servidor servidor = response.readEntity(new GenericType<Servidor>() {
+			});
+
+			return servidor;
+		}
+
+		return null;
+
+	}
+
+	@Override
+	public String getAsString(FacesContext context, UIComponent componente,
+			Object value) {
+
+		Servidor servidor = (Servidor) value;
+		
+		if (servidor.getPessoaId() != 0) {
+
+			return String.valueOf(servidor.getPessoaId());
+
+		}
+		
+		return null;
+	}
+
+}
