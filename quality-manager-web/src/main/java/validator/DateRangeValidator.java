bc4cc7cf91987a26aabd67209c899b0d348741bf
rhavymaia <rmg3@cin.ufpe.br>
2015-07-07 21:20:56 -0300

    - Submissão do edital: validação das datas.

---
+++ quality-manager-web/src/main/java/validator/DateRangeValidator.java
@@ -0,0 +1,33 @@
+package validator;
+ 
+import java.util.Date;
+
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.validator.FacesValidator;
+import javax.faces.validator.Validator;
+import javax.faces.validator.ValidatorException;
+ 
+@FacesValidator("dateRangeValidator")
+public class DateRangeValidator implements Validator {
+     
+    @Override
+    public void validate(FacesContext context, UIComponent component, Object value) throws ValidatorException {
+        if (value == null) {
+            return;
+        }
+         
+        //Leave the null handling of startDate to required="true"
+        Object startDateValue = component.getAttributes().get("startDate");
+        if (startDateValue==null) {
+            return;
+        }
+         
+        Date startDate = (Date)startDateValue;
+        Date endDate = (Date)value; 
+        if (endDate.before(startDate)) {
+        	// Tratamento de erro caso a data final seja menor que a inicial.
+        	System.out.println("Data inválida.");
+        }
+    }
+}
